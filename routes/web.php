b4d5fac38d29ece85c6dd9c037df1a20bf23bd1d
thanhnhn1108 <thanhnhn1108@gmail.com>
2020-07-15 16:05:17 +0700

    zxczxc

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 123123123;
+    // return view('welcome');
 });
 
 Route::prefix('admin')->group(function(){
