138ed1c509192aa3128d3bc623df8e00e4b8c5d1
thanhnhn1108 <thanhnhn1108@gmail.com>
2020-07-15 15:55:49 +0700

    add theme

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 1123;
+    return view('welcome');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/',function(){
+        return view('admin.index');;
+    })->name('trangchu');
 });
